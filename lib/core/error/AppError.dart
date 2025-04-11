eb37b75421534e98e1d0003644775d3ba78f4fa7
Cường Châu <cuongchautech14@gmail.com>
2025-04-11 15:56:06 +0700

    [project structure] Implement AppError in lib/core

---
+++ lib/core/error/AppError.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:weather_app_assignment/data/exception/DataException.dart';
+
+
+
+class AppError {
+  String? title;
+  String? message;
+  String? errorCode;
+  String? debugMessage;
+  dynamic extraJson;
+  bool isNetworkException = false;
+
+  AppError({
+    this.title,
+    this.errorCode,
+    this.message,
+    this.extraJson,
+    this.debugMessage,
+  });
+
+  @override
+  String toString() =>
+      "title:${title}\nerrorMessage:${message}\nerrorCode:${errorCode}\ndebugMessage:${debugMessage}\nextraJson:${extraJson.toString()}";
+
+
+
+  AppError.fromPlatformException(PlatformException platformException) {
+    title = platformException.code;
+    errorCode = platformException.code;
+    message = platformException.message;
+    extraJson = platformException.details;
+  }
+
+  AppError.fromDataException(DataException dataException) {
+    title = dataException.code;
+    errorCode = dataException.code;
+    message = dataException.message;
+    extraJson = dataException.extraJson;
+  }
+}
