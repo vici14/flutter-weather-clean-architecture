d6ec850c8a5b3284dd57726d2c4f6177a3960a9f
Cường Châu <cuongchautech14@gmail.com>
2025-04-11 16:00:35 +0700

    [project structure] Setup project dependency injection by GetIt

---
+++ lib/core/dependency_injection/service_locator.dart
@@ -0,0 +1,51 @@
+import 'package:get_it/get_it.dart';
+import '../../data/repositories/i_weather_repository.dart';
+import '../../data/repositories/weather_repository.dart';
+import '../../data/repositories/i_location_repository.dart';
+import '../../data/repositories/location_repository.dart';
+import '../../features/weather/bloc/weather_bloc.dart';
+import '../../features/location/bloc/location_bloc.dart';
+import '../../data/service_manager.dart';
+import '../../core/services/firebase_manager.dart';
+import '../../core/services/secure_storage.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> setupServiceLocator() async {
+  try {
+    // Services
+    getIt.registerSingleton<SecureStorage>(SecureStorage());
+
+    getIt.registerSingleton<FirebaseManager>(FirebaseManager());
+    getIt<FirebaseManager>().setSecureStorage(getIt<SecureStorage>());
+    await getIt<FirebaseManager>().initialize();
+
+    // ServiceManager with API keys from SecureStorage
+    getIt.registerSingleton<ServiceManager>(ServiceManager());
+    getIt<ServiceManager>().initialize(
+      locationApiKey: await getIt<FirebaseManager>().getLocationApiKey(),
+      weatherApiKey: await getIt<FirebaseManager>().getWeatherApiKey(),
+    );
+
+    // Repositories
+    getIt.registerLazySingleton<IWeatherRepository>(
+      () => WeatherRepository(getIt<ServiceManager>()),
+    );
+
+    getIt.registerLazySingleton<ILocationRepository>(
+      () => LocationRepository(getIt<ServiceManager>()),
+    );
+
+    // Cubits
+    getIt.registerFactory<WeatherBloc>(
+      () => WeatherBloc(getIt<IWeatherRepository>()),
+    );
+
+    // Blocs
+    getIt.registerFactory<LocationBloc>(
+      () => LocationBloc(getIt<ILocationRepository>()),
+    );
+  } catch (e) {
+    print("setupServiceLocator error:${e.toString()}");
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'theme/theme.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app_assignment/features/weather/bloc/weather_bloc.dart';
+import 'core/theme/theme.dart';
+import 'features/location/bloc/location_bloc.dart';
 import 'routes/app_routes.dart';
+import 'core/dependency_injection/service_locator.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Setup dependency injection
+  await setupServiceLocator();
+
   runApp(const MyApp());
 }
 
@@ -13,12 +22,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Weather App',
-      theme: AppTheme.lightTheme,
-      onGenerateRoute: AppRoutes.onGenerateRoute,
-      initialRoute: AppRoutes.home,
-      debugShowCheckedModeBanner: !kReleaseMode,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<LocationBloc>(
+          create: (context) => getIt<LocationBloc>(),
+        ),
+        BlocProvider<WeatherBloc>(
+          create: (context) => getIt<WeatherBloc>(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Weather App',
+        theme: AppTheme.lightTheme,
+        onGenerateRoute: AppRoutes.onGenerateRoute,
+        initialRoute: AppRoutes.home,
+        debugShowCheckedModeBanner: !kReleaseMode,
+      ),
     );
   }
 }
