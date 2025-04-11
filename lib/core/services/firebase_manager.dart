b0691ec877cfbab84e9580ed1ff484974ab09b06
Cường Châu <cuongchautech14@gmail.com>
2025-04-11 15:57:45 +0700

    [project structure][lib/core] Add FirebaseManager handling initialize & setup remote config

--- lib/core/services/firebase_manager.dart
+++ lib/core/services/firebase_manager.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
-import 'package:weather_app_assignment/core/config/app_config.dart';
 import 'package:weather_app_assignment/firebase_options.dart';
+import 'package:weather_app_assignment/core/services/secure_storage.dart';
 
 class FirebaseManager {
   static final FirebaseManager _instance = FirebaseManager._internal();
+  late final SecureStorage _secureStorage;
 
   factory FirebaseManager() {
     return _instance;
@@ -12,22 +13,15 @@ class FirebaseManager {
 
   FirebaseManager._internal();
 
-  String locationApiKey = '';
-  String weatherApiKey = '';
+  void setSecureStorage(SecureStorage secureStorage) {
+    _secureStorage = secureStorage;
+  }
 
   Future<void> initialize() async {
     // Initialize Firebase
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
-
-    // Skip fetching from Remote Config if in mock test mode
-    if (AppConfig().isMockTest) {
-      // Use default values for mock testing
-      locationApiKey = 'mock_location_api_key';
-      weatherApiKey = 'mock_weather_api_key';
-      return;
-    }
 
     // Initialize Firebase Remote Config
     final remoteConfig = FirebaseRemoteConfig.instance;
@@ -45,11 +39,17 @@ class FirebaseManager {
     // Fetch and activate remote config
     await remoteConfig.fetchAndActivate();
 
-    // Store API keys
-    locationApiKey = remoteConfig.getString('location_api_key');
-    weatherApiKey = remoteConfig.getString('weather_api_key');
+    // Store API keys in secure storage
+    final locationApiKey = remoteConfig.getString('location_api_key');
+    final weatherApiKey = remoteConfig.getString('weather_api_key');
+
+    await _secureStorage.saveLocationApiKey(locationApiKey);
+    await _secureStorage.saveWeatherApiKey(weatherApiKey);
   }
 
-  String getLocationApiKey() => locationApiKey;
-  String getWeatherApiKey() => weatherApiKey;
+  Future<String> getLocationApiKey() async =>
+      (await _secureStorage.getLocationApiKey()) ?? '';
+
+  Future<String> getWeatherApiKey() async =>
+      (await _secureStorage.getWeatherApiKey()) ?? '';
 }
